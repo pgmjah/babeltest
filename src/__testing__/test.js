f9a51b26b7e0f378c4b5d3274050a400ae5efd1e
pgmjah <pgmjah@gmail.com>
2020-11-17 17:14:21 -0500

    added tests

--- src/__testing__/test.js
+++ src/__testing__/test.js
@@ -1,4 +1,14 @@
+import jest from 'jest';
 import Foo, {Bar, Baz} from '../foo.js';
-console.log(Foo(1, 2));
-console.log(Bar(1, 2));
-console.log(Baz(1, 2));
+
+test('Make sure Foo works properly', ()=>{
+	expect(Foo(1, 2)).toBe('Foo: Bar: Baz: 3');
+});
+
+test('Make sure Bar works properly', ()=>{
+	expect(Bar(1, 2)).toBe('Bar: Baz: 3');
+});
+
+test('Make sure Baz works properly', ()=>{
+	expect(Baz(1, 2)).toBe('Baz: 3');
+});
